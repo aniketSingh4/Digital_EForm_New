af5e1284f7a0c6975a7d252c913d5de4f173123b
aniket singh <ankitparihartu98@gmail.com>
2026-07-31 18:01:34 +0530

    Soving NPM Build Problem.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { NotificationProvider } from './context/notificationContext';
+import { NotificationProvider } from './context/NotificationContext';
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
